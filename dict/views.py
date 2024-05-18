8ef4aff062704f5aaf35a57064a8aa3f86c70f51
Srishti Agrawal <shrishtibindki@gmail.com>
2024-05-18 14:20:52 +0530

    added form with post method

--- dict/views.py
+++ dict/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def homePage(request):
     if request.method=='POST':
-        keyword=request.GET["keyword"]
+        keyword=request.POST.get('keyword')
         print(keyword)
     return render(request,'dict/home.html',{})
