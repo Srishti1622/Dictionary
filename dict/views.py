c70ba646faa96c6ec967ea405516d05b0bce6cfe
Srishti Agrawal <shrishtibindki@gmail.com>
2024-05-21 10:26:25 +0530

    applying beautiful soup

--- dict/views.py
+++ dict/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+import requests
 
 # Create your views here.
 def homePage(request):
     if request.method=='POST':
         keyword=request.POST.get('keyword')
         print(keyword)
+
+        url=f"https://www.google.com/search?q={keyword}+meaning"
+        resp=requests.get(url)
+        soup=BeautifulSoup(resp.text,'lxml')
+        print(soup.text)
+        # print(soup.find_all('div',class_="vmod"))
+        # print(soup.find_all('ol',class_="eQJLDd"))
+
+        
     return render(request,'dict/home.html',{})
